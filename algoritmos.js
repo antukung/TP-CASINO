849794b588218585d92a9b7d20bffab8edb38a79
Antu <122485403+antukung@users.noreply.github.com>
2023-07-23 15:28:10 -0300

    segundo commit

--- algoritmos.js
+++ algoritmos.js
@@ -4,6 +4,8 @@ const cuerpoPagina=document.querySelector("body");
 const div1=document.querySelector("#imagen-1");
 const div2=document.querySelector("#imagen-2");
 const div3=document.querySelector("#imagen-3");
+const lista=document.querySelector("#lista");
+
 
 let saldoPesos=parseFloat(prompt ("ingrese la cantidad de pesos que quiere jugar"));
 let nombreJuego=prompt ("ingresesu nombre de jugador");
@@ -25,29 +27,38 @@ const IMG_6="https://img2.freepng.es/20180503/vow/kisspng-business-product-sampl
 const imgVector=[IMG_1,IMG_2,IMG_3,IMG_4,IMG_5,IMG_6];
 
 function verifica() {
-     generarImagen();
-     generarLista();
+    const numeroAleatorio = imagen1();
+    const numeroAleatorio2 = imagen2();
+    const numeroAleatorio3 = imagen3();
+    generarLista(numeroAleatorio, numeroAleatorio2, numeroAleatorio3);
      
 }
 
-function generarImagen() {
-    imagen1();
-    imagen2();
-    imagen3();
-    
-}
+
 function imagen1() {
     let numeroAleatorio = Math.floor(Math.random() * 6);
     div1.innerHTML = `<img src="${imgVector[numeroAleatorio]}">`;
+    return numeroAleatorio;
 }
 
 function imagen2() {
     let numeroAleatorio2 = Math.floor(Math.random() * 6);
     div2.innerHTML = `<img src="${imgVector[numeroAleatorio2]}">`;
+    return numeroAleatorio2;
 }
 
 function imagen3() {
     let numeroAleatorio3 = Math.floor(Math.random() * 6);
     div3.innerHTML = `<img src="${imgVector[numeroAleatorio3]}">`;
+    return numeroAleatorio3;
 }
 
+function generarLista(numeroAleatorio, numeroAleatorio2, numeroAleatorio3) {
+    if (numeroAleatorio === numeroAleatorio2 && numeroAleatorio2 === numeroAleatorio3) {
+        lista.innerHTML += `<p>TENES 3 IMAGENES REPETIDAS</p>`;
+    } else if (numeroAleatorio === numeroAleatorio2 || numeroAleatorio2 === numeroAleatorio3 || numeroAleatorio === numeroAleatorio3) {
+        lista.innerHTML += `<p>TENES 2 IMAGENES REPETIDAS</p>`;
+    } else {
+        lista.innerHTML += `<p>NO TENES NINGUNA IMAGEN REPETIDA</p>`;
+    }
+}
